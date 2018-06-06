e3fd9d2fa2d2e3e93f1e0240510c9882dceb7a88
TheLurkingDev <thelurkingdev@gmail.com>
2018-06-05 21:58:49 -0400

    Create the User controller and pass the dbContext via constructor DI.

---
+++ CoreBB.Web/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreBB.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBB.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private CoreBBContext _dbContext;
+
+        public UserController(CoreBBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
